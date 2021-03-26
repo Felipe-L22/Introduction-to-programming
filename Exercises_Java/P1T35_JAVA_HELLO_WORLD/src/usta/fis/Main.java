508a392d74c17f1a021ef80b4a58c2348c2527e0
Felipe <vfelipe2227@gmail.com>
2021-03-26 09:43:39 -0500

    Java

---
+++ Exercises_Java/P1T35_JAVA_HELLO_WORLD/src/usta/fis/Main.java
@@ -0,0 +1,17 @@
+package usta.fis;
+//AUTHOR: Felipe Lopez
+//DATE: 2021-March-16
+//DESCRIPTION: this is my first program in java
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------");
+        System.out.println("softhellowordl version 1.0");
+        System.out.println("Maker: Felipe Lopez G");
+        System.out.println("Description: This software show HELLO WORLD");
+        System.out.println("---------------------------");
+        System.out.println("hello world");
+    }
+}
